b396c9d6ea39c4f5fbed7e19fa6c0502737cf350
Vaillus <hugovaillaud@gmail.com>
2025-10-16 10:36:18 +0200

    Refactor LinkedIn job scraping logic
    - Updated the main function to utilize both primary and secondary queries for job scraping.
    - Introduced a list of requests to handle multiple locations and remote types.
    - Enhanced the scraping process to iterate through requests and queries, improving flexibility in job searches.

--- src/jobseeker_agent/scraper/linkedin_scraper.py
+++ src/jobseeker_agent/scraper/linkedin_scraper.py
@@ -190,22 +190,65 @@ class LinkedInJobsScraper:
 def main():
 
     builder = QueryBuilder()
-    query = builder.build_secondary_query()
-    
-    print(query)
-    remote_type = "any" # remote, hybrid, on_site, any
-    max_time = "month" # day, week, month
-    params = {
-        "keywords": query,
-        "location": "Paris, France",
-        "max_jobs": 100,
-        "remote_type": remote_type,
-        "max_time": max_time
-    }
+    # query = builder.build_secondary_query()
+    query = builder.build_primary_query()
 
+    requests = [
+        {
+            "location": "Sidney, Australia",
+            "remote_type": "any"
+        },
+        {
+            "location": "Australia",
+            "remote_type": "remote"
+        },
+        {
+            "location": "Paris, France",
+            "remote_type": "any"
+        },
+        {
+            "location": "France",
+            "remote_type": "remote"
+        },
+        {
+            "location": "Germany",
+            "remote_type": "remote"
+        },
+        {
+            "location": "Amsterdam, Netherlands",
+            "remote_type": "any"
+        },
+        {
+            "location": "Netherlands",
+            "remote_type": "remote"
+        },
+        {
+            "location": "California, USA",
+            "remote_type": "remote"
+        }
+    ]
+    queries = [
+        builder.build_primary_query(), 
+        builder.build_secondary_query()
+    ]
     scraper = LinkedInJobsScraper()
-    new_jobs_added = scraper.scrape_jobs(**params)
-    print(f"Finished scraping. Added {new_jobs_added} new jobs.")
+    for request in requests:
+        for query in queries:
+            params = {
+                "keywords": query,
+                "location": request["location"],
+                "max_jobs": 100,
+                "remote_type": request["remote_type"],
+                "max_time": "week"
+            }
+            new_jobs_added = scraper.scrape_jobs(**params)
+            print(f"Finished scraping. Added {new_jobs_added} new jobs.")
+    
+    
+
+    # scraper = LinkedInJobsScraper()
+    # new_jobs_added = scraper.scrape_jobs(**params)
+    # print(f"Finished scraping. Added {new_jobs_added} new jobs.")
 
 
 if __name__ == "__main__":
